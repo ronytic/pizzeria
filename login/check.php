e6ff4b27029c0dd33a78a1eae369da2a420b96ec
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-28 21:48:46 -0400

    Login Check

--- login/check.php
+++ login/check.php
@@ -7,6 +7,7 @@ $dir=dirname(__FILE__).DIRECTORY_SEPARATOR."../";
 define("RAIZ",$dir);
 include_once($_SERVER['DOCUMENT_ROOT']."/".$subfolder."/config.php");
 include_once(RAIZ."rastreo/revisar.php");
+include_once(RAIZ."funciones/funciones.php");
 if(!(isset($_SESSION["login"]) && $_SESSION['login']==1)){
 	header("Location:$url$directory/login/?u=".$_SERVER['PHP_SELF']);
 }
