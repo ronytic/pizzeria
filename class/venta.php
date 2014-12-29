ba0a8ac51c072c173f25ee6727e2ca288e73cad9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-28 21:48:40 -0400

    Clase Venta

--- class/venta.php
+++ class/venta.php
@@ -2,6 +2,9 @@
 include_once("bd.php");
 class venta extends bd{
 	var $tabla="venta";
-
+	function mostrar($where,$campoorder,$order){
+		$where=$where!=""?$where." and Activo=1":"Activo=1";
+		return $this->getRecords($where,$campoorder,"",0,0,$order);
+	}
 }
 ?>
