b8157b97549b05c41cbd598264ffb8d84d24a8f6
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-30 11:25:16 -0400

    CabeceraHtml

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <meta name="description" content="">
     <meta name="author" content="Ronald Franz Nina Layme">
     <link rel="icon" href="<?php echo $folder?>imagenes/favicon.ico">
