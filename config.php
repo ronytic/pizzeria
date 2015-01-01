ec682ee3cfa5cf02b83ab5ac00a63ee865f8b1fd
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-01 19:29:41 -0400

    Init Session

--- config.php
+++ config.php
@@ -9,4 +9,6 @@ $lema='';
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
 $tp="D3W1N8E0E4B4B4";
+ini_set('session.bug_compat_warn', 0);
+ini_set('session.bug_compat_42', 0);
 ?>
