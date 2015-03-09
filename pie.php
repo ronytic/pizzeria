6056610e958ca4c68e84e8246681de615c063b49
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-03-09 19:37:25 -0400

    Corregido Pie de Página

--- pie.php
+++ pie.php
@@ -1,7 +1,7 @@
  <!-- FOOTER -->
       <footer class="blancot">
         <p class="pull-right">Sistema Desarrollado por: <a href="http://facebook.com/ronaldnina" target="_blank" title="Cel: 73230568">Ronald Nina Layme</a> &middot; </p>
-        <p>Sistema de Venta de Pizzeria - Todos los Derechos Reservados &copy; <?php echo date("Y");?> </p>
+        <p>Sistema de Venta de Pizzas - Todos los Derechos Reservados &copy; <?php echo date("Y");?> </p>
       </footer>
 
     </div><!-- /.container -->
